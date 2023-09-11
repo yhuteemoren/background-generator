fc7a80caca008310d07d0107927802f0b3490f4c
yhuteemoren <utibeabasiumoren1@gmail.com>
2023-09-11 14:40:13 +0100

    modified node bundles

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+var _ = require("lodash");
+
+var array = [1, 2, 4, 4, 3, 5, ,];
+
+console.log("answer", _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
